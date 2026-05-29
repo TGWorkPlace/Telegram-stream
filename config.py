7d97085583960066a01f6dfd13cf83e1c5467593
TGWorkPlace <DramaShip.org@gmail.com>
2026-05-29 19:27:23 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+
+API_ID = int(os.environ.get("API_ID", 0))
+API_HASH = os.environ.get("API_HASH", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+
+RTMP_URL = os.environ.get("RTMP_URL", "")
+STREAM_KEY = os.environ.get("STREAM_KEY", "")
+
+OWNER_ID = int(os.environ.get("OWNER_ID", 0))
+
+PORT = int(os.environ.get("PORT", 8080))
