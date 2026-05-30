16794345ba211c6368a68277d98b5a77c3552f03
TGWorkPlace <DramaShip.org@gmail.com>
2026-05-30 16:39:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ API_ID = int(os.environ.get("API_ID", 0))
 API_HASH = os.environ.get("API_HASH", "")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 MONGO_URI = os.environ.get("MONGO_URI", "")
+ADMINS = [int(id) for id in os.environ.get("ADMINS", "").split(",")]
 
 RTMP_URL = os.environ.get("RTMP_URL", "")
 STREAM_KEY = os.environ.get("STREAM_KEY", "")
